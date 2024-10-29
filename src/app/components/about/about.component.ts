ba731ce500b1144019c4821cd217feca32c992cd
Karani Wachira <wachirakeith@gmail.com>
2024-10-29 13:05:25 +0300

    feat: add query params in the about route

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -15,11 +15,19 @@ export class AboutComponent implements OnInit{
   ) {
   }
 
-  param: any;
-
-  ngOnInit(): void {
-    console.log(this.activatedRoute);
-    this.param = this.activatedRoute.snapshot.params['username'];
-  }
+  // param: any;
+  // queryParam: any;
+  //
+  // ngOnInit(): void {
+  //   console.log(this.activatedRoute);
+  //   this.param = this.activatedRoute.snapshot.params['username'];
+  //   this.queryParam = this.activatedRoute.snapshot.queryParams['course'];
+  // }
 
 }
+
+
+
+
+
+
